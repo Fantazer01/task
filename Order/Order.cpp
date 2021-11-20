c7ce3051fc810e2bc7566698d975cf28b3a34611
Oleg <olegkrygin9@gmail.com>
2021-11-20 23:40:10 +0300

    forgot "return stream"...

--- Order/Order.cpp
+++ Order/Order.cpp
@@ -6,8 +6,10 @@
 
 std::istream& operator >> (std::istream& input, Order &obj) {
     input >> obj.id >> obj.number;
+    return input;
 }
 
 std::ostream& operator << (std::ostream& output, const Order &obj) {
     output << obj.id << " " << obj.number;
+    return output;
 }
